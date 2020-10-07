7bd1f8764b0861a4662f8c0006b1c27a7438d856
Alejandro <alejandro2hpxa@gmail.com>
2020-10-07 10:16:36 -0500

    blue

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ console.log(new Date().toString());
 console.log(chalk.red`it's ogre`);
 console.log(chalk.green`it's ogre`);
 console.log(chalk.yellow`it's ogre`);
+console.log(chalk.blue`it's ogre`);
